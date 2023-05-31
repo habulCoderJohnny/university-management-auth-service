5858e90bf1509f30b356299a78743a97232be6c6
habulCoderJohnny <fokruljohnny@gmail.com>
2023-05-31 22:29:33 +0600

    Setup [Done] ESLint | prettier | husky | lint-stage

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
-import mongoose from "mongoose";
-import config from "./config/index"; 
-import app from "./app";
-
+import mongoose from 'mongoose'
+import config from './config/index'
+import app from './app'
 
 // connectivity
 async function server() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log("♻️Database connected✅");
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('♻️Database connected✅')
 
-     app.listen(config.port, () => {
-        console.log(`Application app listening on port ${config.port}`)
-     });
-
-    } catch(err){
-      console.log("Failed connect to database 🚫", err);
-    }  
+    app.listen(config.port, () => {
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Failed connect to database 🚫', err)
+  }
 }
-server();
+server()
