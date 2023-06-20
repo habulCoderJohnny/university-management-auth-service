9798b74232c20ba943c9353673349b60c624de3e
habulCoderJohnny <fokruljohnny@gmail.com>
2023-06-20 19:46:25 +0600

    [Done] AC faculty Pagination, Sorting Searching and Filtering

--- src/apps/modules/AcademicFaculties/faculty.controller.ts
+++ src/apps/modules/AcademicFaculties/faculty.controller.ts
@@ -4,6 +4,9 @@ import sendResponse from '../../../shared/sendResponse';
 import { IACFaculty } from './faculty.interface';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
+import pgPicker from '../../../shared/pgPicker';
+import { paginationField } from '../../../constant/pagination';
+import { facultyFilterableFields } from './faculty.constant';
 
 // createController
 const createACFaculty = catchAsync(async (req: Request, res: Response) => {
@@ -18,13 +21,31 @@ const createACFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 // getAllController
-const getAllFaculty = catchAsync(async (req: Request, res: Response) => {
+/* const getAllFaculty = catchAsync(async (req: Request, res: Response) => {
   const result = await ACFacultyService.getAllFaculties();
   sendResponse<IACFaculty[]>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: '✅All Faculty retrieved successfully!',
     data: result,
+  });
+}); */
+
+// filtering & pagination & sort
+const getAllFaculty = catchAsync(async (req: Request, res: Response) => {
+  const filters = pgPicker(req.query, facultyFilterableFields); //filtering
+  const paginationOptions = pgPicker(req.query, paginationField);
+  const result = await ACFacultyService.getAllFaculties(
+    filters,
+    paginationOptions
+  );
+
+  sendResponse<IACFaculty[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: '✅All Faculty retrieved successfully!',
+    meta: result.meta,
+    data: result.data,
   });
 });
 
--- src/apps/modules/AcademicFaculties/faculty.service.ts
+++ src/apps/modules/AcademicFaculties/faculty.service.ts
@@ -1,5 +1,10 @@
 import { AcademicFaculty } from './faculty.model';
-import { IACFaculty } from './faculty.interface';
+import { IACFaculty, IACFacultyFilters } from './faculty.interface';
+import { IPaginationOptions } from '../../../pagination/typePagination';
+import { IGenericResponse } from '../../../interfaces/common';
+import { facultySearchableFields } from './faculty.constant';
+import { paginationSort } from '../../../pagination/paginationSort';
+import { SortOrder } from 'mongoose';
 
 // create service
 const createACFaculty = async (payload: IACFaculty): Promise<IACFaculty> => {
@@ -8,9 +13,65 @@ const createACFaculty = async (payload: IACFaculty): Promise<IACFaculty> => {
 };
 
 // get all service
-const getAllFaculties = async (): Promise<IACFaculty[]> => {
-  const result = await AcademicFaculty.find();
-  return result;
+// const getAllFaculties = async (): Promise<IACFaculty[]> => {
+//   const result = await AcademicFaculty.find();
+//   return result;
+// };
+
+// Pagination:searching&filtering+getAllSemester
+const getAllFaculties = async (
+  filters: IACFacultyFilters,
+  paginationOptions: IPaginationOptions
+): Promise<IGenericResponse<IACFaculty[]>> => {
+  //searching
+  const { searchTerm, ...filtersData } = filters;
+  const andConditions = [];
+
+  //searching
+  if (searchTerm) {
+    andConditions.push({
+      // or
+      $or: facultySearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+  //filtering
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      //and q
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationSort.calculatePagination(paginationOptions);
+
+  const sortConditions: { [key: string]: SortOrder } = {};
+
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await AcademicFaculty.find(whereConditions)
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+  const total = await AcademicFaculty.countDocuments();
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
 };
 
 // get one service
