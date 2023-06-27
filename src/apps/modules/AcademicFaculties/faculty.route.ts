e862245edeea57565539b01784b2e23c6f7a9167
habulCoderJohnny <fokruljohnny@gmail.com>
2023-06-28 02:43:45 +0600

    Create and verify token auth middleware and Authorization the academicFaculty: access restricted

--- src/apps/modules/AcademicFaculties/faculty.route.ts
+++ src/apps/modules/AcademicFaculties/faculty.route.ts
@@ -2,20 +2,40 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { ACFacultyController } from './faculty.controller';
 import { ACFacultyValidation } from './faculty.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.post(
   '/create',
   validateRequest(ACFacultyValidation.createFacultyZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   ACFacultyController.createACFaculty
 );
 router.get('/:id', ACFacultyController.getOneFaculty);
-router.get('/', ACFacultyController.getAllFaculty);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  ACFacultyController.getAllFaculty
+);
 router.patch(
   '/update/:id',
   validateRequest(ACFacultyValidation.updateFacultyZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
   ACFacultyController.updateFaculty
 );
-router.delete('/delete/:id', ACFacultyController.deleteFaculty);
+router.delete(
+  '/delete/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  ACFacultyController.deleteFaculty
+);
 export const ACFacultyRoutes = router;
